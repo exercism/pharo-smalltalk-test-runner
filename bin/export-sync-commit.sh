85c0dcaa4a467754179649fd4d4346e7115901ea
Erik Schierboom <erik_schierboom@hotmail.com>
2024-04-04 09:18:17 +0200

    Deploy with sync sha

--- bin/export-sync-commit.sh
+++ bin/export-sync-commit.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
 export SHA_OR_BRANCH=$(sed -n '3p' Pharo-sync-commit)
+
+if [ -n "$GITHUB_ACTIONS" ]; then
+    echo "SHA_OR_BRANCH=${SHA_OR_BRANCH}" >> $GITHUB_ENV
+    echo "SHA_OR_BRANCH=${SHA_OR_BRANCH}" >> $GITHUB_OUTPUT
+fi
